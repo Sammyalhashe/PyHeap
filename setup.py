ffb78145e42c021eb9c8561a204231a35d7c0411
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2020-01-11 13:11:46 -0500

    updating setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyHeap",
-    version="0.0.2",
+    version="0.0.3",
     author="Sammyalhashe",
     author_email="sammy.alhashemi@mail.utoronto.ca",
     description="A small heap implementation with customizeable key",
